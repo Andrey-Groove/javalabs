60951e6293865c0eaaff5c7accd3b1c91f9ac84d
Andrey-Groove <konoplevandrey336@gmail.com>
2025-02-28 15:10:34 +0300

    Привёл main к нормальному состоянию и проверил работу программы

--- Laba2/src/main/java/org/example/Main.java
+++ Laba2/src/main/java/org/example/Main.java
@@ -1,9 +1,29 @@
 package org.example;
 
+import java.util.*;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        ExpressionEvaluator evaluator = new ExpressionEvaluator();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Введите выражение (или 'exit' для выхода): ");
+            String expression = scanner.nextLine();
+
+            if (expression.equalsIgnoreCase("exit")) {
+                break;
+            }
+
+            double result = evaluator.evaluate(expression);
+
+            if (!Double.isNaN(result)) {
+                System.out.println("Результат: " + result);
+            }
+        }
+
+        System.out.println("Программа завершена.");
     }
 }
