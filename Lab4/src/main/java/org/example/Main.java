67ee61514ef95b1b93641be56d33664686c5cae9
Andrey-Groove <konoplevandrey336@gmail.com>
2025-03-25 14:08:39 +0300

    Сделал функцию для проверки чтения из файла и проверил правильность работы

--- Lab4/src/main/java/org/example/Main.java
+++ Lab4/src/main/java/org/example/Main.java
@@ -5,30 +5,39 @@ import com.opencsv.exceptions.CsvException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
+    public static void processCsvFromResources(String csvFilePath) throws IOException, CsvException {
+
+        try (InputStream in = Main.class.getClassLoader().getResourceAsStream(csvFilePath)) {
+            if (in == null) {
+                throw new IOException("Файл не найден: " + csvFilePath);
+            }
+
+            try (CSVReader reader = new CSVReader(new InputStreamReader(in))) {
+                String[] nextLine;
+                while ((nextLine = reader.readNext()) != null) {
+
+                    System.out.println(Arrays.toString(nextLine));
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        String csvFilePath = "foreign_names.csv"; // Путь к файлу в ресурсах
-        char separator = ';'; // Разделитель
+        String csvFilePath = "foreign_names.csv";
 
-        try (InputStream in = Main.class.getClassLoader().getResourceAsStream(csvFilePath);
-             CSVReader reader = (in == null) ? null : new CSVReader(new InputStreamReader(in), separator)) {
 
-            if (reader == null) {
-                throw new FileNotFoundException("Файл не найден: " + csvFilePath);
-            }
+        try {
+            processCsvFromResources(csvFilePath);
 
-            String[] nextLine;
-            while ((nextLine = reader.readNext()) != null) {
-                // Выводим каждую строку на экран
-                System.out.println(Arrays.toString(nextLine));
-            }
-
-        } catch (FileNotFoundException e) {
-            System.err.println("Ошибка: Файл не найден: " + e.getMessage());
         } catch (IOException e) {
             System.err.println("Ошибка ввода/вывода: " + e.getMessage());
+        } catch (CsvException e) {
+            System.err.println("Ошибка при чтении CSV: " + e.getMessage());
         }
     }
 }
